422d0a841ffe02dcb1241fe8e4fb8312950d4617
RonaldTroncoso-Duoc <ro.troncosop@duocuc.cl>
2026-06-13 21:38:19 -0400

    Integracion de server url para todos los ms redireccionando a localhost:8080

--- gestion-clinica/ms-pacientes/ms-pacientes/src/main/java/example/ms_pacientes/config/OpenApiConfig.java
+++ gestion-clinica/ms-pacientes/ms-pacientes/src/main/java/example/ms_pacientes/config/OpenApiConfig.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,6 +17,12 @@ public class OpenApiConfig {
         final String securitySchemeName = "bearerAuth";
 
         return new OpenAPI()
+
+                .addServersItem(
+                        new Server()
+                                .url("http://localhost:8080")
+                                .description("API Gateway")
+                )
 
                 .info(
                         new Info()
